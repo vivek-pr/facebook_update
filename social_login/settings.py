39335d08978b7ec0f84f0a74c73c5066d67e87d8
vivek.pradhan <vivek.pradhan@ansrsource.com>
2017-03-13 15:36:24 +0530

    Add new hosts name

--- social_login/settings.py
+++ social_login/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l($!!#zg_g9*)_13)^ky^cl9vfbyu*7%akjl#@u%8y0x4b!=+9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['52.32.190.82']
+ALLOWED_HOSTS = ['52.32.190.82', 'ec2-52-32-190-82.us-west-2.compute.amazonaws.com']
 
 
 # Application definition
